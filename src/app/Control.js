fcc2bb2ab5da6cc6f0d47b2a976d4a9576fb477c
Sim <0doogadooga@gmail.com>
2023-12-18 15:40:19 +0900

    삭제 완료

--- src/app/Control.js
+++ src/app/Control.js
@@ -1,12 +1,25 @@
 "use client";
 
 import Link from "next/link";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 export function Control() {
   const params = useParams();
+  const router = useRouter();
   // console.log("params :", params);
   const id = params.id;
+
+  const deleteHandler = () => {
+    const option = {
+      method: "DELETE",
+    };
+    fetch(`http://localhost:9999/topics/${id}`, option)
+      .then((resp) => resp.json())
+      .then(() => {
+        router.push("/");
+        router.refresh();
+      });
+  };
   return (
     <ul>
       <li>
@@ -19,7 +32,7 @@ export function Control() {
             <Link href={`/update/${id}`}>Update</Link>
           </li>
           <li>
-            <input type="button" value="delete" />
+            <input type="button" value="delete" onClick={deleteHandler} />
           </li>
         </>
       )}
